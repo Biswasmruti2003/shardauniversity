cc141664d9a46d33d92376609f644d4c0aceb473
biswa <biswasmrutibaral2003@gmail.com>
2026-02-28 14:15:19 +0530

    second

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary sticky-top ">
             <div className="container-fluid px-md-5 py-0 ">
-                <a className="navbar-brand" href="#"><img src="/public/images/unilogo.png" alt="firstlogo" /></a>
+                <a className="navbar-brand" href="#"><img src="/shardauniversity/images/unilogo.png" alt="firstlogo" /></a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
